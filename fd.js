01061f221829fac7672377cd6eec3811f5d07812
Patrick Cool <4230215+Pslice@users.noreply.github.com>
2020-01-08 22:01:32 -0800

    add video

--- fd.js
+++ fd.js
@@ -1,1 +1,10 @@
+const video = document.getElementByID('video');
 
+function startVideo() {
+ navigator.getUserMedia(
+   {  video:{} },
+      stream => video.srcObject = stream,
+      err => console.error(err);
+   )
+}
+startVideo();
