9dc557477f648e6173a22a6eb460112662514b59
Patrick Cool <patrickscool@gmail.com>
2020-01-17 19:56:19 -0800

    canvas added

--- fd.js
+++ fd.js
@@ -18,10 +18,19 @@ function startVideo() {
 video.addEventListener('play', () => {
    const canvas = faceapi.createCanvasFromMedia(video);
    document.body.append(canvas);
-   const displaySize = {}
+   console.log('canvas');
+   const displaySize = { width: video.width, height: video.height };
+   faceapi.matchDimensions(canvas, displaySize);
+   console.log('display');
    setInterval(async () => {
       const detections = await faceapi.detectAllFaces(video,
-         new faceapi.tinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+         new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+      console.log('draw');
    }, 100);
 })
 
